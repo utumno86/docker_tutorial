99010dfb939aa4cb2a2dbfcc4fd770052db547a9
Michael Nash <michael@spatialnetworks.com>
2019-01-08 18:08:21 -0500

    container orchestration

--- DockerKubernetesCourse/fibonacci/server/index.js
+++ DockerKubernetesCourse/fibonacci/server/index.js
@@ -47,7 +47,7 @@ app.get('/values/all', async (req, res) => {
   res.send(values.rows)
 })
 
-app.get('/vales/current', async (req, res) => {
+app.get('/values/current', async (req, res) => {
   redisClient.hgetall('values', (err, values) => {
     res.send(values)
   })
