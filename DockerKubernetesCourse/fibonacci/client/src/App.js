c0704948837eeb9fa447f500ac07dfed575c679a
Michael Nash <michael@spatialnetworks.com>
2019-01-15 13:37:16 -0500

    removed the react logo

--- DockerKubernetesCourse/fibonacci/client/src/App.js
+++ DockerKubernetesCourse/fibonacci/client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import OtherPage from './OtherPage'
@@ -11,7 +10,7 @@ class App extends Component {
       <Router>
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+            <h1>Fibonacci Calculator</h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
             <div>
