ca49a3fe3e66e543e098ec32accbd4d7254fe729
Michael Nash <michael@spatialnetworks.com>
2019-02-07 17:40:01 -0500

    deploy the application

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,18 @@
 #! /bin/bash
 
-docker build -t utumno86/fibonacci-client ./DockerKubernetesCourse/fibonacci/client
-docker build -t utumno86/fibonacci-server ./DockerKubernetesCourse/fibonacci/server
-docker build -t utumno86/fibonacci-worker ./DockerKubernetesCourse/fibonacci/worker
+docker build -t utumno86/fibonacci-client:latest -t utumno86/fibonacci-client:$SHA ./DockerKubernetesCourse/fibonacci/client
+docker build -t utumno86/fibonacci-server:latest -t utumno86/fibonacci-server:$SHA ./DockerKubernetesCourse/fibonacci/server
+docker build -t utumno86/fibonacci-worker:latest -t utumno86/fibonacci-server:$SHA ./DockerKubernetesCourse/fibonacci/worker
 
-docker push utumno86/fibonacci-client
-docker push utumno86/fibonacci-server
-docker push utumno86/fibonacci-worker
+docker push utumno86/fibonacci-client:latest
+docker push utumno86/fibonacci-server:latest
+docker push utumno86/fibonacci-worker:latest
+
+docker push utumno86/fibonacci-client:$SHA
+docker push utumno86/fibonacci-server:$SHA
+docker push utumno86/fibonacci-worker:$SHA
 
 kubectl apply -f ./DockerKubernetesCourse/fibonacci/k8s
+kubectl set image deployments/server-deployment server=utumno86/fibonacci-server:$SHA
+kubectl set image deployments/client-deployment client=utumno86/finonacci-client:$SHA
+kubectl set image deployments/worker-deployment worker=utumno86/fibonacci-worker:$SHA
